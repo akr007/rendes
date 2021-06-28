fe568b6382b0eac57217f49c4b4a004e70dc892e
akr007 <antalkrisztian0@gmail.com>
2021-06-28 12:58:55 +0200

    Can access all the three task through an index file. Added index.php, header.html, navbar.html.

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php session_start(); ?>
+    <?php
+        include("header.html");
+        include("navbar.html");
+    ?>
+    <title>RendesWeb feladat</title>
+</head>
+<body>
+    <div class="container">
+        <h1>RendesWeb interjú feladat</h1>
+    </div>
+</body>
+</html>
